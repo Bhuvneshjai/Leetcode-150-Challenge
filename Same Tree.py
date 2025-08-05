85a2a0220fa8e77f16bb1a4c8f92c71ff9f7a409
Bhuvneshjai <bhuvneshjain2000@gmail.com>
2025-08-05 22:51:45 +0530

    Same Tree

---
+++ Same Tree.py
@@ -0,0 +1,68 @@
+'''
+Same Tree: Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Example 1:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+Example 3:
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+
+Constraints:
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+'''
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# Helper Function
+def build_tree(nodes):
+    if not nodes:
+        return None
+
+    root = TreeNode(nodes[0])
+    queue = [root]
+
+    i = 1
+    while i < len(nodes):
+        current = queue.pop(0)
+        if nodes[i] is not None:
+            current.left = TreeNode(nodes[i])
+            queue.append(current.left)
+
+        i += 1
+        if i < len(nodes) and nodes[i] is not None:
+            current.right = TreeNode(nodes[i])
+            queue.append(current.right)
+
+        i += 1
+    return root
+
+p = build_tree([1,2,3])
+q = build_tree([1,2,3])
+sol = Solution()
+print(sol.isSameTree(p, q))
